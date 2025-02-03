62dcfa33423d5e1dfffbf5bfa952f725062c102d
Marius F. Hummel <contact@marius.li>
2025-02-03 20:56:46 +0100

    Update App.tsx

--- restaurantclient/src/App.tsx
+++ restaurantclient/src/App.tsx
@@ -2,16 +2,13 @@ import './App.sass'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import ReservationPlatform from "./pages/Client/ReservationPlatform/ReservationPlatform.tsx";
 import OwnerSetup from "./pages/Owner/Setup/OwnerSetup.tsx";
-import Tables from "./components/FormObjects/TableCreator/tablesForm.tsx";
+
 function App() {
   return (
     <Router>
       <Routes>
-
         <Route path="/setup" element={<OwnerSetup />} />
         <Route path="/reservations" element={<ReservationPlatform />} />
-        <Route path="/tables" element={<Tables />} />
-
       </Routes>
     </Router>
   )
